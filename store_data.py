0b7498c2bca8cef2b5526959e9bdcb412f0ef232
zhaung921 <94048436+zhaung921@users.noreply.github.com>
2021-11-10 18:35:36 +0800

    Add files via upload

---
+++ store_data.py
@@ -0,0 +1,30 @@
+
+#w = []
+##train_ac = 0
+from train import train
+
+
+#test_ac = 0
+
+
+def getw():
+    global w
+    w = []
+
+
+# def get_w():
+#    return w
+
+
+def grttrain_ac():
+    global train_ac
+    train_ac = 0
+
+
+# def get_train_ac():
+#    return train_ac
+
+
+def gettest_ac():
+    global test_ac
+    test_ac = 0
